ba47cc5194b2feabaef857ccc6668e44970acc4e
Ridwanullah Sanni <bolakale4sanni@gmail.com>
2022-11-10 17:13:51 +0100

    initial commit

---
+++ chapterThree/Account.java
@@ -0,0 +1,40 @@
+public class Account{
+
+
+
+
+
+          private Sting name;
+          private double balance;
+
+
+
+     public Account(){
+
+
+
+}
+
+  public Account(String name, double balance){
+
+
+
+     this.name=name,
+     this.balance=balance;
+
+}
+
+  public String getName(){
+          return name;
+}
+  public void setName(String name) {
+          this.name = name;
+}
+  
+  public double getBalance(){
+          return balance;
+}
+
+
+
+}
